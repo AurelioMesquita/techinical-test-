3330485672f938c78e7d350c88c531e69078e781
aurelio.mesquita <aurelio.mesquita@apoio.agro.gov.br>
2025-04-28 21:44:55 -0300

    Creating new components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { Container } from './components/Container';
+import { Footer } from './components/Footer';
 import { Heading } from './components/Heading';
-
+import './styles/global.css';
+import './styles/theme.css';
 export function App() {
-  return <Heading />;
+  return (
+    <>
+      <Container>
+        <Heading>Heading</Heading>
+      </Container>
+      <Container>
+        <Footer />
+      </Container>
+    </>
+  );
 }
