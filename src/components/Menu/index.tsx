b2b2a231d54c10f2086a223e03a7510113a5dd4e
aurelio.mesquita <aurelio.mesquita@apoio.agro.gov.br>
2025-05-03 18:56:28 -0300

    creating theme dark and light

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,36 @@
-import { HistoryIcon, HouseIcon, SettingsIcon, SunIcon } from 'lucide-react';
+import {
+  HistoryIcon,
+  HouseIcon,
+  MoonIcon,
+  SettingsIcon,
+  SunIcon,
+} from 'lucide-react';
 import styles from './styles.module.css';
+import { useEffect, useState } from 'react';
 
+type availableTheme = 'dark' | 'light';
 export function Menu() {
+  const [theme, setTheme] = useState<availableTheme>(() => {
+    const storageTheme =
+      (localStorage.getItem('theme') as availableTheme) || 'dark';
+    return storageTheme;
+  });
+
+  const handleClickTheme = (
+    e: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+  ) => {
+    e.preventDefault();
+    setTheme(prevTheme => {
+      const nextTheme = prevTheme === 'dark' ? 'light' : 'dark';
+      return nextTheme;
+    });
+  };
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
   return (
     <nav className={styles.menu}>
       <a className={styles.menuLink} href='#'>
@@ -13,8 +42,8 @@ export function Menu() {
       <a className={styles.menuLink} href='#'>
         <SettingsIcon />
       </a>
-      <a className={styles.menuLink} href='#'>
-        <SunIcon />
+      <a className={styles.menuLink} href='#' onClick={handleClickTheme}>
+        {theme === 'dark' ? <SunIcon /> : <MoonIcon />}
       </a>
     </nav>
   );
