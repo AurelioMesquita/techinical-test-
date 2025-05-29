136e52ec8489bc0f92e1374e3cf921468a022298
aurelio.mesquita <aurelio.nogueira.mesquita@hotmail.com>
2025-05-29 17:43:34 -0300

    creating loadBeep

---
+++ src/utils/loadBeep.ts
@@ -0,0 +1,13 @@
+import gravitational_beep from '../assets/audios/gravitational_beep.mp3';
+
+export function loadBeep() {
+  const audio = new Audio(gravitational_beep);
+  audio.load();
+
+  return () => {
+    audio.currentTime = 0;
+    audio.play().catch(error => {
+      console.log('Erro ao pegar o audio', error);
+    });
+  };
+}
